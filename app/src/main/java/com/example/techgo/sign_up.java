6bf46ad8b049ec91fe6500aa61cb7f852cd1addb
lethanhson1504 <51450607+lethanhson1504@users.noreply.github.com>
2021-01-04 22:09:39 +0700

    edit sign_up, sign_up2, layout sign_up

--- app/src/main/java/com/example/techgo/sign_up.java
+++ app/src/main/java/com/example/techgo/sign_up.java
@@ -1,5 +1,6 @@
 package com.example.techgo;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -10,64 +11,56 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.Locale;
-import java.util.Vector;
 
 public class sign_up extends AppCompatActivity {
 
-    private Button Customer_bt;
-    private EditText username_text;
-    private EditText password_text;
-    private EditText email_text;
-    private EditText phone_text;
-    private boolean is_customer = false;
-    private FirebaseAuth mAuth;
-    private FirebaseDatabase database;
+    EditText password_text;
+    EditText email_text;
+    EditText phone_text;
+    String is_customer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        mAuth = FirebaseAuth.getInstance();
-        database = FirebaseDatabase.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         setAppLocale("en");
 
-        Customer_bt = findViewById(R.id.customer_bt);
+        is_customer = "false";
+
+        email_text = findViewById(R.id.email_register_text);
+
+        password_text = findViewById(R.id.pw_register_text);
+
+        phone_text = findViewById(R.id.phone_num_register_text);
+
+        Button Customer_bt = findViewById(R.id.customer_bt);
+
         Customer_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                is_customer = true;
+                is_customer = "true";
             }
-        });
-
-
-        username_text = findViewById(R.id.user_name_register_text);
-        password_text = findViewById(R.id.pw_register_text);
-
-        email_text = findViewById(R.id.email_register_text);
-        phone_text = findViewById(R.id.phone_num_register_text);
+        }) ;
 
         Button bt_next = findViewById(R.id.next_register_bt);
         bt_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //name and password
-                String username = username_text.getText().toString();
-                String password = password_text.getText().toString();
 
-                //email and phone number
-                String email = username_text.getText().toString();
-                String phone = password_text.getText().toString();
-                // User newUser = new User(username,email,password,confirmPassword);
-                User newUser = new User("nmndnsdsd","namnfdhllfe@gmail.com","dsndsishfi","dsndsishfi");
-                Vector<String> arr =  newUser.CreateAccount(database.getReference());
+                String is_cus = is_customer;
+                String email =  email_text.getText().toString();
+                String phone =  phone_text.getText().toString();
+                String pw =  password_text.getText().toString();
 
-                //Intent intent = new Intent();
-                //intent.setClass(sign_up.this, sign_up2.class);
-                //startActivity(intent);
+                Intent intent = new Intent(sign_up.this, sign_up2.class);
+
+                intent.putExtra("PW", pw);
+                intent.putExtra("PHONE", phone);
+                intent.putExtra("EMAIL", email);
+                intent.putExtra("IS_CUS", is_cus);
+
+                startActivity(intent);
             }
         }) ;
     }
--- app/src/main/java/com/example/techgo/sign_up2.java
+++ app/src/main/java/com/example/techgo/sign_up2.java
@@ -1,18 +1,64 @@
 package com.example.techgo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class sign_up2 extends AppCompatActivity {
+import java.io.Serializable;
+
+public class sign_up2 extends AppCompatActivity implements Serializable {
+
+
+    private EditText displayname_text;
+    private EditText address_text;
+    private EditText birthday_text;
+    private Button bt_finish;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up2);
+
+        Intent i =  getIntent();
+        final sign_up data_sign_up = (sign_up)i.getSerializableExtra("data_sign_up");
+
+        displayname_text = findViewById(R.id.display_name_register_text);
+        address_text = findViewById(R.id.address_register_text);
+
+        birthday_text = findViewById(R.id.birth_register_text);
+
+        bt_finish = findViewById(R.id.finish_register_bt);
+
+        bt_finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String birthday = birthday_text.getText().toString();
+                String address = address_text.getText().toString();
+
+                String display_name = displayname_text.getText().toString();
+
+                //password
+                String password = getIntent().getStringExtra("PW");
+
+                //email and phone number
+                String email = getIntent().getStringExtra("EMAIL");
+                String phone = getIntent().getStringExtra("PHONE");
+
+                String is_customer = getIntent().getStringExtra("IS_CUS");
+
+                //boolean to store customer or driver
+                boolean is_cus = false;
+                if(is_customer.equals("true"))
+                    is_cus = true;
+
+            }
+        }) ;
 
         Button bt_pre = findViewById(R.id.previous_register_bt);
         bt_pre.setOnClickListener(new View.OnClickListener() {
@@ -21,6 +67,7 @@ public class sign_up2 extends AppCompatActivity {
                 finish();
             }
         }) ;
+
     }
 
 }
